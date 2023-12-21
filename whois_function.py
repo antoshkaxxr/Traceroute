8d25e3e27c8f8f5571ab1ca8e595b001309000e5
antoshkaxxr <63500718+antoshkaxxr@users.noreply.github.com>
2023-12-21 20:30:16 +0500

    small fix

--- whois_function.py
+++ whois_function.py
@@ -3,7 +3,8 @@ import socket
 
 def whois_func(ip):
     addr = "whois.ripe.net" if ':' in ip else "whois.arin.net"
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) if ':' in ip \
+        else socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
     sock.connect((addr, 43))
     query = f"n {ip}\r\n".encode("utf-8")
     sock.send(query)
